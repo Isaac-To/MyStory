2a2048bc6e59777e417d2529c1f11644174c63cf
Isaac-To <isaacto3890@gmail.com>
2022-05-24 16:58:37 -0700

    image support

--- MyStory/app/models.py
+++ MyStory/app/models.py
@@ -14,10 +14,10 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to=f'app/static/image/', blank=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-
     class Meta:
         ordering = ['-created_on']
 
